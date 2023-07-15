630d3dce9599126d70960561c3e39fab8159355c
Andris Shvarcs <andris.svarcs@yahoo.com>
2023-07-15 05:28:58 +0100

    Add SCSS support and bg image support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
-<div>
-    Hero Image
+<script>
+  import bgHero from '$lib/images/bg-hero.jpg'
+</script>
+
+<svelte:head>
+    <title>Home - Ava Portfolio</title>
+    <meta name="description" content="Portfolio">
+</svelte:head>
+
+<div class="hero">
+    <h1>Avanade X</h1>
+    <p>Some description here</p>
 </div>
 
 <h1>London Digital Studio</h1>
@@ -11,4 +21,11 @@
 <h3>Testimonials</h3>
 <p>Snippets of testimonials, probably some slide</p>
 
-
+<style lang="scss">
+  .hero {
+    display: flex;
+    height: 300px;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
