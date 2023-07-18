ec062c4c9bb8420f3f26bbdd70e1759971012c03
Andris Shvarcs <andris.svarcs@yahoo.com>
2023-07-18 18:48:37 +0100

    Updates

--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import {dev} from '$app/environment';
-
-export const title = 'Avanade';
-export const description = "Avanade's description";
-export const url = dev ? 'http://localhost:5173/' : 'https://outside.source.com/'
