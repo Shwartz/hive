ff4814d33fffc6a5b6f8edd420025317ad95448e
Andris Shvarcs <andris.svarcs@yahoo.com>
2023-07-19 17:53:23 +0100

    Add page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,21 @@
   // Supports weights 300-900
   import "@fontsource-variable/rubik";
   import "./styles.css";
+  import { fly } from "svelte/transition";
+
+  export let data;
 </script>
 
 <div class="app">
   <main>
-    <slot />
+    {#key data.url}
+      <div
+        in:fly={{ y: -200, duration: 300, delay: 300 }}
+        out:fly={{ y: 200, duration: 300 }}
+      >
+        <slot />
+      </div>
+    {/key}
   </main>
 
   <footer>
