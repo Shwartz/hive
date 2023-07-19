094b6403cd47c7b6d0eddd2b31b430a218be1972
Andris Shvarcs <andris.svarcs@yahoo.com>
2023-07-19 11:48:39 +0100

    Improving top animation for circle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-  import {fly} from 'svelte/transition';
-  import {backOut} from 'svelte/easing';
-  import {onMount} from 'svelte';
-  import {base} from "$app/paths";
+  import { fly } from "svelte/transition";
+  import { backOut } from "svelte/easing";
+  import { onMount } from "svelte";
+  import { base } from "$app/paths";
 
-  let animate = false
-  const words = ['London', 'Digital', 'Studio', '🪄'];
+  let animate = false;
+  const words = ["London", "Digital", "Studio", "🪄"];
   export let data;
 
   onMount(() => {
@@ -14,26 +14,26 @@
 </script>
 
 <svelte:head>
-    <title>Home - Ava Portfolio</title>
-    <meta name="description" content="Portfolio">
+  <title>Home - Ava Portfolio</title>
+  <meta name="description" content="Portfolio">
 </svelte:head>
 
 <div class="hero">
-    <div class="circle">
-        <div class="content">
-            <h1>Avanade X</h1>
-            <p>We partnered with EDF Hinkley Point C to build a customized Azure platform, empowering them to transform
-                their business while protecting the security of their data and applications.</p>
-        </div>
+  <div class="circle">
+    <div class="content">
+      <h1>Avanade X</h1>
+      <p>We partnered with EDF Hinkley Point C to build a customized Azure platform, empowering them to transform
+        their business while protecting the security of their data and applications.</p>
     </div>
+  </div>
 </div>
 
 <section class="medium center">
-    <h1 class="lines">&nbsp;
-        {#if animate}
-            {#each words as word, i}
+  <h1 class="lines">&nbsp;
+    {#if animate}
+      {#each words as word, i}
             <span
-                    in:fly|global={{
+              in:fly|global={{
                     delay: 500 * (i + 1),
                     duration: 300,
                     easing: backOut,
@@ -42,29 +42,30 @@
             >
                 {word}
             </span>
-            {/each}
-        {/if}
-    </h1>
+      {/each}
+    {/if}
+  </h1>
 
-    <p>Short description</p>
-    <p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would nicely divide by every screen size.</p>
+  <p>Short description</p>
+  <p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
+    nicely divide by every screen size.</p>
 </section>
 
 <section class="large">
-    <h3 class="center">Our work 2</h3>
-    <div class="cards">
-        {#each data.posts as post}
-            <div class="card">
-                <div class="head">
-                    <img src="{base}/images/{post.image}" alt={post.title}>
-                </div>
-                <div class="content">
-                <h3><a href={base}/{post.slug} class="title">{post.title}</a></h3>
-                <p class="description">{post.description}</p>
-                </div>
-            </div>
-        {/each}
-    </div>
+  <h3 class="center">Our work 2</h3>
+  <div class="cards">
+    {#each data.posts as post}
+      <div class="card">
+        <div class="head">
+          <img src="{base}/images/{post.image}" alt={post.title}>
+        </div>
+        <div class="content">
+          <h3><a href={base}/{post.slug} class="title">{post.title}</a></h3>
+          <p class="description">{post.description}</p>
+        </div>
+      </div>
+    {/each}
+  </div>
 </section>
 
 <h3>Testimonials</h3>
@@ -81,6 +82,7 @@
   }
 
   .circle {
+    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -91,12 +93,23 @@
     border: 1px solid rgba(255, 255, 255, 0.5);
     box-shadow: 0 0 20px white;
     border-radius: 50%;
-    background: linear-gradient(90deg, rgba(0, 0, 0, 0.6) 0%, rgba(85, 85, 85, 0.2) 100%);
     color: white;
-    animation: rotation 10s infinite linear;
+
+    &:before {
+      content: "";
+      position: absolute;
+      border-radius: 50%;
+      height: 100%;
+      width: 100%;
+      top: 0;
+      left: 0;
+      background: linear-gradient(90deg, rgba(0, 0, 0, 0.7) 10%, rgba(85, 85, 85, 0.1) 100%);
+      animation: rotation 12s infinite linear;
+    }
 
     .content {
-      animation: rotation 10s infinite linear reverse;
+      position: relative;
+      /*animation: rotation 10s infinite linear reverse;*/
     }
 
     h1 {
@@ -122,7 +135,7 @@
 
   .cards {
     display: grid;
-    grid-template-columns: repeat(1,minmax(0,1fr));
+    grid-template-columns: repeat(1, minmax(0, 1fr));
     gap: 1rem;
 
     .card {
@@ -150,15 +163,15 @@
     }
 
     @media (min-width: 480px) {
-      grid-template-columns: repeat(2,minmax(0,1fr));
+      grid-template-columns: repeat(2, minmax(0, 1fr));
     }
 
     @media (min-width: 800px) {
-      grid-template-columns: repeat(3,minmax(0,1fr));
+      grid-template-columns: repeat(3, minmax(0, 1fr));
     }
 
     @media (min-width: 1024px) {
-      grid-template-columns: repeat(4,minmax(0,1fr));
+      grid-template-columns: repeat(4, minmax(0, 1fr));
     }
   }
 </style>
--- src/routes/[slug]/+layout.svelte
+++ src/routes/[slug]/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
   import './../styles.css';
+  import { base } from "$app/paths";
 </script>
 
 <div class="app">
     <header>
-        Back to <a href="/">Home</a>
+        Back to <a href="{base}/">Home</a>
     </header>
     <main>
         <slot />
