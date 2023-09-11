893178224d3dd7353e91c1b75ec0606848a4f21f
Andris Shvarcs <andris.svarcs@yahoo.com>
2023-09-11 06:28:23 +0100

    Section What is Avanade

--- src/componets/pages/WhatIsAvanade.svelte
+++ src/componets/pages/WhatIsAvanade.svelte
@@ -1,27 +1,45 @@
-<section id="whatIsAvanade" class="medium light">
+<section id="whatIsAvanade" class="large light">
   <div>
     <h2 class="brandTitle center">What is Avanade X?</h2>
-
-    <p>Short description</p>
-    <p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-      nicely divide by every screen size.</p>
-    <p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-      nicely divide by every screen size.</p>
-    <p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-      nicely divide by every screen size.</p>
-    <p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-      nicely divide by every screen size.</p>
-    <p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-      nicely divide by every screen size.</p><p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-    nicely divide by every screen size.</p>
-    <p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-      nicely divide by every screen size.</p><p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-    nicely divide by every screen size.</p><p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-    nicely divide by every screen size.</p><p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-    nicely divide by every screen size.</p><p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-    nicely divide by every screen size.</p><p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-    nicely divide by every screen size.</p><p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-    nicely divide by every screen size.</p><p>By the way, if we could get 12 portfolio cases it would look awesome on a screen. The reason is that it would
-    nicely divide by every screen size.</p>
+    <div class="cols">
+      <div class="col">
+        <h3>Digital Agency</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.</p>
+      </div>
+      <div class="col">
+        <h3>Global Presence</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.</p>
+      </div>
+      <div class="col">
+        <h3>One Team</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.</p>
+      </div>
+    </div>
+    <img src="$lib/images/map.png" alt="world map">
   </div>
 </section>
+
+<style lang="scss">
+  .cols {
+    display: flex;
+    gap: 6rem;
+    margin-top: 4rem;
+  }
+
+  .col {
+    text-align: center;
+
+    h3 {
+      font-family: var(--font-bold);
+      font-size: 2rem;
+    }
+  }
+  img {
+    display: block;
+    width: 80%;
+    margin: 2rem auto;
+  }
+</style>
